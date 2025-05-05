15454faa62c61a3507b9f0b4fed0028c1e0352fe
RicardoAD17 <ricardoalmada036@gmail.com>
2025-05-04 23:05:49 -0600

    a

--- proyecto-angular-temp/src/app/app.component.ts
+++ proyecto-angular-temp/src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './componentes/header/header.component';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, FooterComponent,NavbarComponent,HeaderComponent],
