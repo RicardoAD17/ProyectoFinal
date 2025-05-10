c5efa80ce2babc071aa62fb6faeb40b938a7e2b8
yeyocenteno <yeyocenteno@hotmail.com>
2025-05-10 15:25:14 -0600

    cambios en el header para el carrusel y restruturacion de la app

--- proyecto-angular-temp/src/app/componentes/home/home.component.ts
+++ proyecto-angular-temp/src/app/componentes/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { OfertadepComponent } from '../ofertadep/ofertadep.component';
 import { GaleriaComponent } from '../galeria/galeria.component';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [GaleriaComponent,OfertadepComponent],
+  imports: [GaleriaComponent,OfertadepComponent, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
