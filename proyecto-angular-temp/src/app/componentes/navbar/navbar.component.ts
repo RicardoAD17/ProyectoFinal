9fd571adc7be62e007dada258bcbe315bac421f0
RicardoAD17 <ricardoalmada036@gmail.com>
2025-05-09 23:17:07 -0600

    inicio

--- proyecto-angular-temp/src/app/componentes/navbar/navbar.component.ts
+++ proyecto-angular-temp/src/app/componentes/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone:true,
-  imports: [MatButtonModule,MatMenuModule],
+  imports: [MatButtonModule,MatMenuModule,CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
