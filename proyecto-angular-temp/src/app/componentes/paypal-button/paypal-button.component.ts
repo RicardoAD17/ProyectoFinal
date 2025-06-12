33f9747557c318799ef93376adde7bf4736693b5
RicardoAD17 <ricardoalmada036@gmail.com>
2025-06-12 09:26:39 -0600

    api's paypal

---
+++ proyecto-angular-temp/src/app/componentes/paypal-button/paypal-button.component.ts
@@ -0,0 +1,33 @@
+import { AfterViewInit, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paypal-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './paypal-button.component.html',
+  styleUrl: './paypal-button.component.css'
+})
+export class PaypalButtonComponent implements AfterViewInit{
+ ngAfterViewInit(): void {
+    // @ts-ignore
+    paypal.Buttons({
+      createOrder: (data: any, actions: any) => {
+        return actions.order.create({
+          purchase_units: [{
+            amount: {
+              value: '10.00' // Monto de la transacción
+            }
+          }]
+        });
+      },
+      onApprove: (data: any, actions: any) => {
+        return actions.order.capture().then((details: any) => {
+          alert(`Transacción completada por ${details.payer.name.given_name}`);
+        });
+      },
+      onError: (err: any) => {
+        console.error('Error en la transacción:', err);
+      }
+    }).render('#paypal-button-container');
+  }
+} 
