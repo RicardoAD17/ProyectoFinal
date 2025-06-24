91621dd02d206d7f3542431054b325290203d620
RicardoAD17 <ricardoalmada036@gmail.com>
2025-06-24 16:47:21 -0600

    qrcomponente

--- proyecto-angular-temp/src/app/componentes/qr/qr.component.ts
+++ proyecto-angular-temp/src/app/componentes/qr/qr.component.ts
@@ -1,22 +1,24 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';  // Import necesario para ngModel
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import * as QRCode from 'qrcode';
 
 @Component({
   selector: 'app-qr',
   standalone: true,
-  imports: [CommonModule, HttpClientModule]
+  imports: [CommonModule, HttpClientModule, FormsModule],
+  templateUrl: './qr.component.html',
 })
 export class QrComponent {
 
+  textoQR: string = '';  // Propiedad para ngModel
   @ViewChild('qrCanvas', { static: true }) canvas!: ElementRef<HTMLCanvasElement>;
 
   constructor(private http: HttpClient) {}
 
   obtenerDatos() {
-    const userId = 'usuario123';  // Este puede venir de login o localStorage
-
+    const userId = 'usuario123';
     this.http.get(`http://localhost:3000/api/qr-data/${userId}`).subscribe({
       next: (res: any) => {
         const datos = JSON.stringify(res);
@@ -29,7 +31,7 @@ export class QrComponent {
   generarQR(texto: string) {
     QRCode.toCanvas(this.canvas.nativeElement, texto, { width: 256 }, error => {
       if (error) console.error(error);
-      else console.log('QR generado con datos del API');
+      else console.log('QR generado');
     });
   }
 }
