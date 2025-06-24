91196d60fa5b56a9c94c5409c22bccf8a5573307
valmichi <deconwaypato@gmail.com>
2025-06-24 14:18:09 -0600

    Add package-lock.json and update app routes formatting

--- proyecto-angular-temp/src/app/app.routes.ts
+++ proyecto-angular-temp/src/app/app.routes.ts
@@ -7,10 +7,10 @@ import { TablasComponent } from './componentes/tablas/tablas.component';
 import { CursoDetalleComponent } from './componentes/curso-detalle/curso-detalle.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent }, 
- { path: 'curso/:id', component: CursoDetalleComponent},
+  { path: 'curso/:id', component: CursoDetalleComponent},
   { path: 'curso', component: CursoComponent},
   { path: 'nosotros', component: NosotrosComponent},
   { path: 'suscripcion', component: SuscripcionComponent },
   { path: 'tablas', component: TablasComponent },
-  { path: '**', redirectTo: '' },
+  { path: '**', redirectTo: '' }
 ];
